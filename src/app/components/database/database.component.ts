cb4e22d4d7d3cba6d97fb074b427e002274d8cda
Emmanuel_Vargas <manu.vargasc2004@gmail.com>
2022-06-24 00:08:41 -0300

    database update n2

--- src/app/components/database/database.component.ts
+++ src/app/components/database/database.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs'
 
-export interface Item { name: string; }
+export interface Item { title: string; author: string; content: string }
 
 @Component({
   selector: 'app-database',
@@ -14,7 +14,7 @@ export class DatabaseComponent implements OnInit {
   private itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
   constructor(private afs: AngularFirestore) {
-    this.itemsCollection = afs.collection<Item>('items');
+    this.itemsCollection = afs.collection<Item>('posts');
     this.items = this.itemsCollection.valueChanges();
   }
   addItems(item: Item){
